eb52634a074a1d6866a45e5afebfc1b6817572b2
NguyenSy <guensai197@gmail.com>
2020-08-04 13:46:58 +0700

    Revert "Sy sieu cap vo dich dep trai"
    This reverts commit 94a230e494bb77cf6d0db083d2bec5c9e6488b2a.

--- DoAn/app/src/main/java/com/example/ssenjoymusicapp/Fragment/Fragment_DiaNhac.java
+++ DoAn/app/src/main/java/com/example/ssenjoymusicapp/Fragment/Fragment_DiaNhac.java
@@ -34,11 +34,10 @@ public class Fragment_DiaNhac extends Fragment {
         view =  inflater.inflate(R.layout.fragment_dia_nhac,container,false);
         circleImageView = view.findViewById(R.id.imageviewCircle);
         objectAnimator = objectAnimator.ofFloat(circleImageView,"rotation",0f,360f);
-        objectAnimator.setDuration(20000);
+        objectAnimator.setDuration(1000);
         objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
         objectAnimator.setRepeatMode(ValueAnimator.RESTART);
         objectAnimator.setInterpolator(new LinearInterpolator());
-        objectAnimator.start();
         return view;
     }
     public void PLayNhac(String hinh){
